930e6494e97ad1b506b467fd072330d59dce0db2
lachoaiphan <lhphan@ucdavis.edu>
2020-08-11 17:56:17 -0700

    Updates

---
+++ precache-manifest.e10c6ba92ba5433effc89f34da1f6fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82960ec0105ebbddb1c5fc52c041a3f",
+    "url": "/TicTacToeOnTheWeb.github.io/index.html"
+  },
+  {
+    "revision": "7e10d217587fc59b8ec0",
+    "url": "/TicTacToeOnTheWeb.github.io/static/css/main.b0064f3d.chunk.css"
+  },
+  {
+    "revision": "44b9640b073a467e9776",
+    "url": "/TicTacToeOnTheWeb.github.io/static/js/2.18f1bbbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToeOnTheWeb.github.io/static/js/2.18f1bbbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e10d217587fc59b8ec0",
+    "url": "/TicTacToeOnTheWeb.github.io/static/js/main.be7b8bcb.chunk.js"
+  },
+  {
+    "revision": "4a9eef5facd6cacffdfe",
+    "url": "/TicTacToeOnTheWeb.github.io/static/js/runtime-main.62a9ab51.js"
+  }
+]);
